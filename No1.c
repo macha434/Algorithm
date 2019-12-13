d579304adb7b650b7c2e2e978da5fea50af6c5f6
macha434 <tennis16434@gmail.com>
2019-12-13 15:10:21 +0900

    error

--- No1.c
+++ No1.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+void Check_setup();
 void Set_Up();
 void Register();
 void Product_Add();
 void Price_Change();
 void Check_Money();
 void Poweroff();
+void Format();
+void Print_Product();
 
 struct data{
     char name[20];
@@ -35,10 +38,10 @@ int main(){
 
     product = root;
 
-    Set_Up(product, mon);
+    Check_setup(product, mon);
 
     while(1){
-        printf("\n\nMODE SELECT\n");
+        printf("\nMODE SELECT\n");
         printf("1:Register, 2:Produt Add, 3:Price Change, 4:Check money, 5:poweroff, 6:Reset : ");
         scanf("%d", &i);
 
@@ -64,7 +67,7 @@ int main(){
                 break;
 
             case 6:
-                format();
+                Format(root);
                 break;
 
             default:
@@ -77,33 +80,33 @@ int main(){
     return 0;
 }
 
-void check_setup(void){
+void Check_setup(void){
 
-	int check;
-	FILE *check_setupfile;
+    int check;
+    FILE *check_setupfile;
 
-	check_setupfile = fopen("setup.txt", "r");
-	if(check_setupfile == NULL) {
-		printf("OK\n");
-		check_setupfile = fopen("setup.txt", "w");
-		fprintf(check_setupfile, "0\n");
-		fclose(check_setupfile);
-		check_setupfile = fopen("setup.txt", "r");
-	}
-	fscanf(check_setupfile, "%d", &check);
-	fclose(check_setupfile);
+    check_setupfile = fopen("setup.txt", "r");
+    if(check_setupfile == NULL) {
+        printf("OK\n");
+        check_setupfile = fopen("setup.txt", "w");
+        fprintf(check_setupfile, "0\n");
+        fclose(check_setupfile);
+        check_setupfile = fopen("setup.txt", "r");
+    }
+    fscanf(check_setupfile, "%d", &check);
+    fclose(check_setupfile);
 
-	printf("%d",check);
+    printf("%d",check);
 
-	if(check==0){
+    if(check==0){
         Set_Up();
         return;
-	}else if(check==1){
-		return;
-	}else{
-		printf("Error\n");
-		exit(0);
-	}
+    }else if(check==1){
+        return;
+    }else{
+        printf("Error\n");
+        exit(0);
+    }
 }
 
 
@@ -111,18 +114,18 @@ void Set_Up(struct data *product, struct money mon[]){
 
     Product_Add(product);
 
-	FILE *fpw = fopen("struct.dat", "wb");
-	fwrite(&set, sizeof(set), 1, fpw);
-	fclose(fpw);
+    FILE *fpw = fopen("struct.dat", "wb");
+    fwrite(product, sizeof(product), 1, fpw);
+    fclose(fpw);
 
-	FILE *setupfile;
-	setupfile = fopen("setup.txt", "w");
-	if(setupfile == NULL) {
-		printf("cannot open\n");
-		exit(1);
-	}
-	fprintf(setupfile, "1\n");
-	fclose(setupfile);
+    FILE *setupfile;
+    setupfile = fopen("setup.txt", "w");
+    if(setupfile == NULL) {
+        printf("cannot open\n");
+        exit(1);
+    }
+    fprintf(setupfile, "1\n");
+    fclose(setupfile);
 
     strcpy(mon[0].money_name, "one");
     strcpy(mon[1].money_name, "five");
@@ -134,11 +137,15 @@ void Set_Up(struct data *product, struct money mon[]){
     strcpy(mon[7].money_name, "five thousand");
     strcpy(mon[8].money_name, "ten thousand");
 
+    printf("1");
+
     for (int i = 0; i < 8; i++){
 
         printf("How Many %s Yen : ", mon[i].money_name);
         scanf("%d", &mon[i].money_num);
     }
+
+    Print_Product(product);
 
     return;
 }
@@ -185,6 +192,8 @@ void Product_Add(struct data *product){
     product -> next = stor;
     stor -> prev = product;
 
+    /* Print_Product(stor); */
+
     return;
 }
 
@@ -207,12 +216,26 @@ void Poweroff(struct data *root){
     exit (0);
 }
 
-void format(struct data *root){
-	FILE *format_setup;
+void Format(struct data *root){
+    FILE *format_setup;
 
-	format_setup = fopen("setup.txt", "w");
-	fprintf(formar_file, "0\n");
-	fclose(format_setup);
+    format_setup = fopen("setup.txt", "w");
+    fprintf(format_setup, "0\n");
+    fclose(format_setup);
 
-    poweroff(root);
+    return;
+}
+
+void Print_Product(struct data *root){
+
+
+    struct data *product = root;
+
+    for(int i = 0; product != root; product = product -> next, i++){
+
+        printf("%d , Name %s , Price %d , Stock %d\n", i, product -> name, product -> price, product -> stock);
+
+    }
+
+    return;
 }
